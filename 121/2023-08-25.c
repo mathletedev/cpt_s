5555f3cbd1cc5ced30d9388ea3a5a4080d57a789
Neal Wang <nealwang.sh@protonmail.com>
2023-08-28 09:11:09 -0700

    initial commit - first 2 classes

---
+++ 121/2023-08-25.c
@@ -0,0 +1,16 @@
+/*
+ * Name: Neal Wang
+ * Date: 2023-08-25
+ * Class: CPT_S 121
+ * Lab section: 08
+ * Description: Output to the screen a message that says
+ *              "Hello, CPT_S 121!"
+ */
+
+#include <stdio.h> // contains utility functions to output text to screen - printf()
+
+int main(void) { // starting point of execution for every C program
+	printf("Hello, CPT_S 121!");
+
+	return 0;
+}

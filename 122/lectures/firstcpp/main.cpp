61118c986b0f6822972a34f542b33956cfb6b87e
Neal Wang <nealwang.sh@protonmail.com>
2024-02-07 17:07:35 -0800

    C++ time bois

---
+++ 122/lectures/firstcpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Enter a number: ";
+
+	int number;
+	std::cin >> number;
+
+	std::cout << "Your number is " << number << std::endl;
+
+	return 0;
+}

8d6cf6be0d1fdc29e19fcf35fc8b94b10e77b6ee
Neal Wang <nealwang.sh@protonmail.com>
2024-04-04 16:36:25 -0700

    sfml install and use

--- 122/labs/sfml-test/src/main.cpp
+++ 122/labs/sfml-test/src/main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
 
 int main() {
 	sf::RenderWindow window;
 	window.create(sf::VideoMode(500, 500, 32), "SFML Test");
+
+	sf::Texture person_texture;
+	person_texture.loadFromFile("static/person.jpg");
+
+	sf::Sprite person_sprite;
+	person_sprite.setTexture(person_texture);
+	person_sprite.setPosition(sf::Vector2f(250, 250));
+	person_sprite.setOrigin(sf::Vector2f(150, 150));
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -11,6 +20,8 @@ int main() {
 			if (event.type == sf::Event::Closed) window.close();
 
 			window.clear();
+
+			window.draw(person_sprite);
 
 			window.display();
 		}
